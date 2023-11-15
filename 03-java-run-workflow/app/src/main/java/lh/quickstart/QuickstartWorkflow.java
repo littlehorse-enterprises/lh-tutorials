a399f30b0c68a1fdcfb8591b21a97ab9c049949d
sohinipattanayak <sohinipattanayak.it@gmail.com>
2023-11-15 17:28:12 +0530

    added comments and cleaned code

--- 03-java-run-workflow/app/src/main/java/lh/quickstart/QuickstartWorkflow.java
+++ 03-java-run-workflow/app/src/main/java/lh/quickstart/QuickstartWorkflow.java
@@ -6,20 +6,36 @@ import io.littlehorse.sdk.wfsdk.WfRunVariable;
 import io.littlehorse.sdk.wfsdk.Workflow;
 import io.littlehorse.sdk.wfsdk.WorkflowThread;
 
+/**
+ * The QuickstartWorkflow class defines the workflow for the Quickstart application.
+ * It includes the steps and tasks that are part of the workflow.
+ */
 public class QuickstartWorkflow {
 
+    // Constants representing the workflow name and the task name
     public static final String WF_Name = "quickstart";
     public static final String GREET = "greet";
 
-    public void quickstartWf(WorkflowThread wf){
-
+    /**
+     * Defines the logic of the 'quickstart' workflow.
+     * @param wf The workflow thread used to define the workflow steps.
+     */
+    public void quickstartWf(WorkflowThread wf) {
+        // Create and set up an input variable for the workflow
         WfRunVariable name = wf.addVariable("input-name", VariableType.STR);
+        // Enable searching for workflows based on the value of the 'input-name' variable
+        // and make it persistent in the workflow's database
         name.withIndex(IndexType.REMOTE_INDEX).persistent();
+        // Execute the 'greet' task using the 'name' variable
         wf.execute(GREET, name);
-
-}
-    public Workflow getWorkflow(){
-        return Workflow.newWorkflow(WF_Name, this::quickstartWf);
     }
 
+    /**
+     * Creates and returns a new Workflow object for the 'quickstart' workflow.
+     * @return A Workflow object representing the defined workflow.
+     */
+    public Workflow getWorkflow() {
+        // Construct and return a new workflow using the defined workflow logic
+        return Workflow.newWorkflow(WF_Name, this::quickstartWf);
+    }
 }
